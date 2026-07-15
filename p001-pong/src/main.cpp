b6c1deb8a7ba837a02520f373fac37bffd787090
drfdev <drafff@gmail.com>
2026-07-15 21:13:15 +0200

    p001: create simple window with render

--- p001-pong/src/main.cpp
+++ p001-pong/src/main.cpp
@@ -1,5 +1,37 @@
-#include "SDL3/SDL.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     SDL_Log("Success");
+    
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+    SDL_Event event;
+    
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("Hello SDL", 320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
+
+    while (1) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_EVENT_QUIT) {
+            break;
+        }
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
 }
